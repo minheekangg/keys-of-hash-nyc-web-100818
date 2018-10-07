7f7b71701ee81d8d94d904bed21442c42bbf79d2
Min Hee  Park <minheekangg@gmail.com>
2018-10-07 03:41:42 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(*arguments)
     new_array = []
     self.each do |k,v|
-      if arguments.include?
+      if arguments.include?(v)
         new_array << k
       end
       end
